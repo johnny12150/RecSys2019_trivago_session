6390b3e61292676038505b1dc08728ae1bd510ca
Wade <johnny12150@gmail.com>
2020-05-13 17:29:11 +0800

    check重複點擊的量

--- impression.py
+++ impression.py
@@ -1,3 +1,4 @@
+#%%
 import pandas as pd
 from tqdm import tqdm
 from metrics import mean_reciprocal_rank
@@ -9,11 +10,15 @@ test_seesion = pd.read_csv("data/test.csv")
 # 只保留 type是click的資料
 train_click = train_seesion.loc[train_seesion.action_type == 'clickout item', ['user_id', 'session_id', 'reference', 'impressions']]
 train_click['impressions'] = train_click['impressions'].apply(lambda x: x.split('|'))
+
+#%%
 # 觀察是不是 next-click都在 impression內
 s_id = ''
 first_step = True  # 判斷是不是session的開頭
+previous_impressions = ''
 previous_click = ''
 pos_sum = 0
+same_sum = 0 # 下一刻是不是點一樣
 pred_count = 0
 for i, data in tqdm(train_click.iterrows()):
     # 檢查有沒有換session
@@ -21,17 +26,25 @@ for i, data in tqdm(train_click.iterrows()):
         first_step = False
         pred_count += 1
         # 看前一刻的 click是不是在現在的impression內
-        # print(previous_click in data['impressions'])
-        if previous_click in data['impressions']:
+        if data['reference'] in previous_impressions:
             pos_sum += 1
+        elif previous_click == data['reference']:
+            same_sum += 1
     else:
         s_id = data['session_id']
         first_step = True
     previous_click = data['reference']
+    previous_impressions = data['impressions']
 
 # 算有多少比例是從impression點擊的
 print(pos_sum/ pred_count)
+print(same_sum/ pred_count)
+print((pos_sum+same_sum)/ pred_count)
 
+#%%
+# 挑出全部 session倒數兩個 click
+train_click['reference'] = train_click['reference'].astype(str)
+train_click['refs'] = train_click.groupby(['session_id'])['reference'].apply(','.join)
 
 # 計算 baseline MRR
 # 1. random猜
@@ -39,3 +52,6 @@ print(pos_sum/ pred_count)
 # 2. 從 impression裡面 random挑
 
 # 3. 直接猜前一刻的 click
+
+
+# %%
